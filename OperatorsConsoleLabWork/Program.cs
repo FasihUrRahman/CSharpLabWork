d152f64a8a40fb80e32d9b1690e76536e28bb15a
FasihUrRahman <fasihurrahmandeveloper@gmail.com>
2023-04-12 11:30:07 +0500

    Add project files.

---
+++ OperatorsConsoleLabWork/Program.cs
@@ -0,0 +1,40 @@
+﻿//Arthmatic Operators
+Console.Write("10 + 9 = ");
+Console.WriteLine(10 + 9); //Plus Operator
+Console.Write("10 - 9 = ");
+Console.WriteLine(10 - 9); //Minus Operator
+Console.Write("10 x 9 = ");
+Console.WriteLine(10 * 9); //Multiply Operator
+Console.Write("10 / 9 = ");
+Console.WriteLine(10 / 9); //Devide Operator
+Console.Write("10 % 9 = ");
+Console.WriteLine(10 % 9); //Modules Operator
+Console.Write("Hit Enter Key For Next Message.....");
+Console.ReadLine();
+//Comparison Operators
+//This will Give's Answer in Boolean
+Console.WriteLine(10 > 9); //Means: 10 Greater Than 9
+Console.WriteLine(10 < 9); //Means: 10 Smaller Than 9
+Console.WriteLine(10 == 9); //Means: 10 Equals To 9
+Console.WriteLine(10 >= 9); //Means: 10 Greater Than Or Equal To 9
+Console.WriteLine(10 <= 9); //Means: 10 Smaller Than Or Equal To 9
+Console.WriteLine(10 != 9); //Means: 10 Not Equal To 9
+Console.Write("Hit Enter Key For Next Message.....");
+Console.ReadLine();
+//Logical Operators
+//These Operators use to build logics. This Will Give's Answer in Boolean.
+Console.WriteLine((10 > 9) && (15 < 20)); //Means: 10 Greater Than 9 AND 15 Smaller Than 20. Which is True.
+Console.WriteLine((10 > 9) && (15 > 20)); //Means: 10 Greater Than 9 AND 15 Greater Than 20. Which is Fales.
+Console.WriteLine((10 > 9) || (15 < 20)); //Means: 10 Greater Than 9 OR 15 Smaller Than 20. Which is True.
+                                          //Because Both Are True.
+Console.WriteLine((10 > 9) || (15 > 20)); //Means: 10 Greater Than 9 OR 15 Smaller Than 20. Which is True.
+                                          //Because One of Them Are True.
+Console.WriteLine((10 < 9) || (15 < 20) && (25 < 10) || !(60 > 40)); //Means: 10 Smaller Than 9 OR 15 Smaller
+                                                                     //Then 20 AND 25 Smaller Than 10 OR 60
+                                                                     //NOT Greater Than 40. Which is True.
+                                                                     //Because One of them is True.
+Console.Write("Hit Enter Key For Next Message.....");
+Console.ReadLine();
+Console.WriteLine("Thanks For Joining Me. Good Bye");
+Console.WriteLine("Hit Any Key to Exit.");
+Console.ReadKey();
---
+++ ProjectConsole/Program.cs
@@ -0,0 +1,8 @@
+﻿//Console.WriteLine("Hello, Sir!");
+//Console.Write("This is Fasih Ur Rahman. One of Your New Student. ");
+//Console.WriteLine("I am " + 23 + " Year Old. and I worked as a Wordpress E-Commerace Developer and a Professional Wedding PhotoGrapher. ");
+//Console.WriteLine("Here are some easy math solutions.");
+//Console.Write("20 + 60 = ");
+//Console.Write(20 + 60);
+//Console.ReadKey();
+
