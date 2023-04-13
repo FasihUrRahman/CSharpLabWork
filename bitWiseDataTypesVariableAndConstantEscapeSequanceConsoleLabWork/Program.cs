7486d3402cfeddfcd307b1f6314b8d98c4fbc255
FasihUrRahman <fasihurrahmandeveloper@gmail.com>
2023-04-13 09:36:35 +0500

    Update File 13-04-2023

--- bitWiseDataTypesVariableAndConstantEscapeSequanceConsoleLabWork/Program.cs
+++ bitWiseDataTypesVariableAndConstantEscapeSequanceConsoleLabWork/Program.cs
@@ -25,5 +25,24 @@ Console.WriteLine("My Age is " + age); //Concatination with string
 int price;  //Data Type Integer
 //Initilization of a variable
 price = 100;
-Console.WriteLine("Todays Soap Price is " + price);
+Console.WriteLine("Todays Soap Price is " + price); //Concatination with string
+float marksPercentage;
+marksPercentage = 60.65F; //Add Float Value with Capital F in the end of number
+Console.WriteLine("I obtained " + marksPercentage + "% out of 100%");
+double mapCoordinatX , mapCoordinatY; //Decleration of Two Variables
+mapCoordinatX = 0.498721356423; //double add more numbers then float after dot
+mapCoordinatY = 0.482973237234;
+Console.WriteLine("My Current Location's Coordinates are " + mapCoordinatX + "X " + mapCoordinatY + "Y");
+char firstLaterOfMyName;
+firstLaterOfMyName = 'F'; //Char can Store only one charater
+Console.WriteLine("My Name is Fasih and the First Word of my Name is " + firstLaterOfMyName);
+string collegeName = "The Tips"; //string Can Store as Many Charaters as We Want
+Console.WriteLine("My College Name is " + collegeName);
+mapCoordinatX = 0.742937423683;//Variables can be change at any time according to requirement
+mapCoordinatY = 0.742856294239;
+Console.WriteLine("Now My New Current Location's Coordinates are " + mapCoordinatX + "X " + mapCoordinatY + "Y");
+
+//Constant
+const string companyName = "AgencyPrism"; //const is short form of Constant and can not be changed after once it's value done
+Console.WriteLine("My Company's Name is " + companyName + ". In " + companyName + " We all work together.");
 Console.ReadKey();
