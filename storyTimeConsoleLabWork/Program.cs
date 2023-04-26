e86173ad7147784d30e87121ff61c0753a4095fe
FasihUrRahman <fasihurrahmandeveloper@gmail.com>
2023-04-26 09:34:34 +0500

    Update 26-04-2023

--- storyTimeConsoleLabWork/Program.cs
+++ storyTimeConsoleLabWork/Program.cs
@@ -1,11 +1,14 @@
-﻿Console.WriteLine("\t\t\t\tShort Story(The Student)");
-Console.WriteLine("\tAT first the weather was fine and still. The thrushes were calling, " +
+﻿string tabSpace = "\t\t\t\t";
+string newLine = "\n";
+string tab = "\t";
+string heading = "Short Story(The Student)";
+string contentParagraph1 = "AT first the weather was fine and still. The thrushes were calling, " +
     "and in the swamps close by something alive droned pitifully with a sound like blowing " +
     "into an empty bottle. A snipe flew by, and the shot aimed at it rang out with a gay, resounding " +
     "note in the spring air. But when it began to get dark in the forest a cold, penetrating wind blew " +
     "inappropriately from the east, and everything sank into silence. Needles of ice stretched across the " +
-    "pools, and it felt cheerless, remote, and lonely in the forest. There was a whiff of winter.\n" +
-    "\tIvan Velikopolsky, the son of a sacristan, and a student of the clerical academy, returning home " +
+    "pools, and it felt cheerless, remote, and lonely in the forest. There was a whiff of winter." + newLine + tab +
+    "Ivan Velikopolsky, the son of a sacristan, and a student of the clerical academy, returning home " +
     "from shooting, walked all the time by the path in the water-side meadow. His fingers were numb and " +
     "his face was burning with the wind. It seemed to him that the cold that had suddenly come on had " +
     "destroyed the order and harmony of things, that nature itself felt ill at ease, and that was why " +
@@ -20,12 +23,17 @@ Console.WriteLine("\tAT first the weather was fine and still. The thrushes were 
     "same desperate poverty and hunger, the same thatched roofs with holes in them, ignorance, " +
     "misery, the same desolation around, the same darkness, the same feeling of oppression—all " +
     "these had existed, did exist, and would exist, and the lapse of a thousand years would make life " +
-    "no better. And he did not want to go home.");
-Console.WriteLine("\tThe gardens were called the widows' because they were kept by two widows, " +
+    "no better. And he did not want to go home.";
+string contentParagraph2 = "The gardens were called the widows' because they were kept by two widows, " +
     "mother and daughter. A camp fire was burning brightly with a crackling sound, throwing out " +
     "light far around on the ploughed earth. The widow Vasilisa, a tall, fat old woman in a man's " +
     "coat, was standing by and looking thoughtfully into the fire; her daughter Lukerya, a little " +
     "pock-marked woman with a stupid-looking face, was sitting on the ground, washing a caldron and spoons." +
     " Apparently they had just had supper. There was a sound of men's voices; it was the " +
-    "labourers watering their horses at the river.");
+    "labourers watering their horses at the river.";
+string goodByeMessage = "Press Any Key to end...";
+Console.WriteLine(tabSpace + heading);
+Console.WriteLine(tab + contentParagraph1);
+Console.WriteLine(tab + contentParagraph2);
+Console.Write(goodByeMessage);
 Console.ReadKey();
