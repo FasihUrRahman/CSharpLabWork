f67ba2fe768bf251fdd1c5c3f0ca166d70271ecb
FasihUrRahman <fasihurrahmandeveloper@gmail.com>
2023-05-02 13:10:50 +0500

    Update 02-05-2023

---
+++ dataTypesMainAll/Program.cs
@@ -0,0 +1,44 @@
+﻿//DataTypes
+//Number Data Type
+    //Integer Data Type
+int numberInt = 30, maxValueInt = int.MaxValue, miniValueInt = int.MinValue;
+
+Console.WriteLine("DataType Integer: " + numberInt);
+Console.WriteLine("DataType Integer Maximum Value: " + maxValueInt);
+Console.WriteLine("DataType Integer Minimum Value: " + miniValueInt);
+
+    //Byte Data Type
+byte numberByte = 30, maxValueByte = byte.MaxValue, miniValueByte = byte.MinValue;
+
+Console.WriteLine("DataType Byte: " + numberByte);
+Console.WriteLine("DataType Byte Maximum Value: " + maxValueByte);
+Console.WriteLine("DataType Byte Minimum Value: " + miniValueByte);
+
+    //Float Data Type
+float numberFloat = 30.0f, maxValueFloat = float.MaxValue, miniValueFloat = float.MinValue;
+
+Console.WriteLine("DataType Float: " + numberFloat);
+Console.WriteLine("DataType Float Maximum Value: " + maxValueFloat);
+Console.WriteLine("DataType Float Minimum Value: " + miniValueFloat);
+
+    //Double Data Type
+double numberDouble = 30, maxValueDouble = double.MaxValue, miniValueDouble = double.MinValue;
+
+Console.WriteLine("DataType Double: " + numberDouble);
+Console.WriteLine("DataType Double Maximum Value: " + maxValueDouble);
+Console.WriteLine("DataType Double Minimum Value: " + miniValueDouble);
+
+    //Short Data Type
+short numberShort = 30, maxValueShort = short.MaxValue, miniValueShort = short.MinValue;
+
+Console.WriteLine("DataType Short: " + numberShort);
+Console.WriteLine("DataType Short Maximum Value: " + maxValueShort);
+Console.WriteLine("DataType Short Minimum Value: " + miniValueShort);
+
+    //Long Data Type
+long numberLong = 30, maxValueLong = long.MaxValue, miniValueLong = long.MinValue;
+
+Console.WriteLine("DataType Long: " + numberLong);
+Console.WriteLine("DataType Long Maximum Value: " + maxValueLong);
+Console.WriteLine("DataType Long Minimum Value: " + miniValueLong);
+
