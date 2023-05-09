27ad80a188dd7e99f14c4e09b9f609755f7cfe6b
FasihUrRahman <fasihurrahmandeveloper@gmail.com>
2023-05-09 12:43:49 +0500

    Update 09-05-2023

---
+++ WhileDoWhileLoop/Program.cs
@@ -0,0 +1,12 @@
+﻿int inputValue, i = 1; //Variables
+Console.WriteLine("\tTable Generator"); //Title Message
+Console.Write("Enter a Number To Generate Table: ");    //Input Message
+inputValue = Convert.ToInt32(Console.ReadLine());   //Input
+//Loop Start
+while (i <= 10) 
+{
+    Console.WriteLine($"{inputValue} x {i} = {inputValue * i}");    //Result
+    i++;    //Increment
+}
+//Loop End
+Console.ReadLine();
