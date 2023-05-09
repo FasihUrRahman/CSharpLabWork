1217bfc648812246733e24a88417ef8bfbbe2e77
FasihUrRahman <fasihurrahmandeveloper@gmail.com>
2023-05-09 15:01:16 +0500

    Update 09-05-2023

--- WhileDoWhileLoop/Program.cs
+++ WhileDoWhileLoop/Program.cs
@@ -2,11 +2,22 @@
 Console.WriteLine("\tTable Generator"); //Title Message
 Console.Write("Enter a Number To Generate Table: ");    //Input Message
 inputValue = Convert.ToInt32(Console.ReadLine());   //Input
-//Loop Start
+//While Loop Start
+Console.WriteLine("Result With While Loop");
 while (i <= 10) 
 {
     Console.WriteLine($"{inputValue} x {i} = {inputValue * i}");    //Result
     i++;    //Increment
 }
-//Loop End
+//While Loop End
+Console.ReadLine();
+//Do While Loop Start
+i =  1; //Rest The Value of 'i'
+Console.WriteLine("Result With Do While Loop");
+do
+{
+    Console.WriteLine($"{inputValue} x {i} = {inputValue * i}");    //Result
+    i++;
+}
+while (i <= 10);    //Condition
 Console.ReadLine();
