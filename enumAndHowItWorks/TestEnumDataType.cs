9436d8ec352db78300aeabe44ebfa500b455800b
FasihUrRahman <fasihurrahmandeveloper@gmail.com>
2023-05-26 11:34:53 +0500

    Update 26-05-2023

--- enumAndHowItWorks/TestEnumDataType.cs
+++ enumAndHowItWorks/TestEnumDataType.cs
@@ -4,4 +4,8 @@
 {
         FirstValue, SecondValue, ThirdValue, FourthValue, FifthValue
 }
+    enum TestEnumDataType1
+    {
+        FirstValue = 10, SecondValue = 100, ThirdValue = 200, FourthValue = 400, FifthValue = 199
+    }
 }
